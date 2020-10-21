d336daae977cba8619c67c29332f304231dd82f1
D3TONAT0R <gdv@swissonline.ch>
2020-10-21 12:02:49 +0200

    Initial commit

---
+++ MinecraftTerrainPostProcessors/BedrockPostProcessor.cs
@@ -0,0 +1,9 @@
+namespace ASCReaderMC.PostProcessors {
+	public class BedrockPostProcessor : MinecraftTerrainPostProcessor {
+
+		public override void ProcessBlock(MinecraftRegionExporter region, int x, int y, int z) {
+			if(y == 0) region.SetBlock(x, 0, z, "bedrock");
+		}
+
+	}
+}
